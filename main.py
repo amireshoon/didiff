ff478c8885b0c182968817a07a07524159b777e0
Amirhossein Meydani <amirhwsin@outlook.com>
2021-05-18 16:15:02 +0430

    Added walker

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import sys
+import sys, os
+
+def compare(first, second):
+    firstbytes = []
+    for root, dirs, files in os.walk(first):
+        for file in files:
+            with open(os.path.join(root, file), "r", encoding="utf8") as auto:
+                    firstbytes.append(auto.read(4096))
 
 def main():
+    compare(sys.argv[1], sys.argv[2])
     pass
 
 if __name__ == '__main__':
